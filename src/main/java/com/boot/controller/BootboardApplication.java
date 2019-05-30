3df2487d19a643edda14df7923ccabfc09c9eedd
Sungmingyu <jkun1123@gmail.com>
2019-05-30 15:54:36 +0900

    start

---
+++ src/main/java/com/boot/controller/BootboardApplication.java
@@ -0,0 +1,13 @@
+package com.boot.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootboardApplication.class, args);
+	}
+
+}
