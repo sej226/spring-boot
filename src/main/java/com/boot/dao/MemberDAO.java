18d8a9855deaae08399f2f0c24f06bc2fa46807e
MoonYongSeong <forever9882@gmail.com>
2019-05-31 16:42:11 +0900

    junit test

--- src/main/java/com/boot/dao/MemberDAO.java
+++ src/main/java/com/boot/dao/MemberDAO.java
@@ -5,11 +5,11 @@ import java.util.List;
 import com.boot.vo.Member;
 
 public interface MemberDAO {
-	public boolean addMember(Member member);
-	public boolean deleteMember(String id);
-	public boolean updateMember(Member member);
-	public List<Member> selectAllMember(); // 전체 검색
-	public Member selectMemberByID(String id); // 아이디로 검색
-	public List<Member> selectHumorByName(String name); // 이름으로 검색
-	public List<Member> selectHumorByGrade(String grade); // 등급으로 검색
+	public boolean addMember(Member member) throws Exception;
+	public boolean deleteMember(String id) throws Exception;
+	public boolean updateMember(Member member) throws Exception;
+	public List<Member> selectAllMember() throws Exception; // 전체 검색
+	public Member selectMemberByID(String id) throws Exception; // 아이디로 검색
+	public List<Member> selectHumorByName(String name) throws Exception; // 이름으로 검색
+	public List<Member> selectHumorByGrade(String grade) throws Exception; // 등급으로 검색
 }
--- src/main/java/com/boot/dao/impl/MemberDAOImpl.java
+++ src/main/java/com/boot/dao/impl/MemberDAOImpl.java
@@ -17,44 +17,45 @@ public class MemberDAOImpl implements MemberDAO{
 	@Autowired
 	SqlSession sqlSession;
 	
+	
 	@Override
-	public boolean addMember(Member member) {
+	public boolean addMember(Member member) throws Exception{
 		int rows = sqlSession.insert(ns+"addMember", member);
 		if(rows <= 0) return false;
 		return true;
 	}
 
 	@Override
-	public boolean deleteMember(String id) {
+	public boolean deleteMember(String id) throws Exception{
 		int rows = sqlSession.delete(ns+"deleteMember", id);
 		if(rows <= 0) return false;
 		return true;
 	}
 
 	@Override
-	public boolean updateMember(Member member) {
+	public boolean updateMember(Member member) throws Exception{
 		int rows = sqlSession.update(ns+"updateMember", member);
 		if(rows <= 0) return false;
 		return true;
 	}
 
 	@Override
-	public List<Member> selectAllMember() {
-		return sqlSession.selectList(ns+"updateMember");
+	public List<Member> selectAllMember() throws Exception{
+		return sqlSession.selectList(ns+"selectAllMember");
 	}
 
 	@Override
-	public Member selectMemberByID(String id) {
+	public Member selectMemberByID(String id) throws Exception{
 		return sqlSession.selectOne(ns+"selectMemberByID", id);
 	}
 
 	@Override
-	public List<Member> selectHumorByName(String name) {
+	public List<Member> selectHumorByName(String name) throws Exception{
 		return sqlSession.selectList(ns+"selectHumorByName", name);
 	}
 
 	@Override
-	public List<Member> selectHumorByGrade(String grade) {
+	public List<Member> selectHumorByGrade(String grade) throws Exception{
 		return sqlSession.selectList(ns+"selectHumorByGrade", grade);
 	}
 
