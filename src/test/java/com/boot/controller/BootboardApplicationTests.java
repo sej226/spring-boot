2e959bd1eff3cd465602479eca71717b40ea0d5d
ShinEunJeong <sej96226@gmail.com>
2019-05-31 15:28:28 +0900

    Create Mapper.xml

--- src/test/java/com/boot/controller/BootboardApplicationTests.java
+++ src/test/java/com/boot/controller/BootboardApplicationTests.java
@@ -1,5 +1,6 @@
 package com.boot.controller;
 
+import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,7 @@ public class BootboardApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		SqlSession sqlsession ;
 	}
 
 }
