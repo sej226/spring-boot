f1df246bc13ec4300d6cfa8df3c5af42ed0ffee4
ShinEunJeong <sej96226@gmail.com>
2019-06-04 18:04:33 +0900

    게시판 CRUD

--- src/main/java/com/boot/controller/MainController.java
+++ src/main/java/com/boot/controller/MainController.java
@@ -16,199 +16,79 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.boot.service.HumorService;
-import com.boot.service.MemberService;
 import com.boot.vo.BoolResult;
 import com.boot.vo.Humor;
-import com.boot.vo.Member;
 
 @CrossOrigin(origins = {"*"}, maxAge = 6000)
 @Controller
 @RequestMapping("/api")
 public class MainController {
-	public static final Logger logger = LoggerFactory.getLogger(MainController.class);
-	
-	@Autowired
-	private HumorService  humorService;
+   public static final Logger logger = LoggerFactory.getLogger(MainController.class);
+   
+   @Autowired
+   private HumorService  humorService;
 
-	@RequestMapping(value = "/addHumor", method = RequestMethod.POST)
-	public ResponseEntity<BoolResult> addHumor(@RequestBody Humor humor) throws Exception {
-		logger.info("1. -------------addHumor-------------"+humor+" : "+new Date());
+   @RequestMapping(value = "/addHumor", method = RequestMethod.POST)
+   public ResponseEntity<BoolResult> addHumor(@RequestBody Humor humor) throws Exception {
+      logger.info("1. -------------addHumor-------------"+humor+" : "+new Date());
 
-		boolean total = humorService.addHumor(humor);
-		BoolResult nr = new BoolResult();
-		nr.setCount(total);
-		nr.setName("addHumor");
-		nr.setState("success");
-		if (!total) {
-			return new ResponseEntity(HttpStatus.NO_CONTENT);
-		}
-		return new ResponseEntity<BoolResult>(nr, HttpStatus.OK);
-	}
-	@RequestMapping(value = "/selectAllHumor", method = RequestMethod.GET)
-	public ResponseEntity<List<Humor>> selectAllHumor() throws Exception {
-		logger.info("1.-------------selectAllHumor-------------"+new Date());
-		List<Humor> humors = humorService.selectAllHumor();
-		if (humors.isEmpty()) {
-			return new ResponseEntity(HttpStatus.NO_CONTENT);
-		}
-		return new ResponseEntity<List<Humor>>(humors, HttpStatus.OK);
-	}
-	
-<<<<<<< HEAD
-	@RequestMapping(value = "/selectHumorByNum/{humorPK}", method = RequestMethod.GET)
-	public ResponseEntity<Humor> selectHumorByNum(@PathVariable int humorPK) throws Exception{
-		logger.info("-------------selectHumorByNum-------------"+new Date());
-		Humor humor = humorService.selectHumorByNum(humorPK);
-		if(humor == null) {
-			return new ResponseEntity(HttpStatus.NO_CONTENT);
-		}
-		return new ResponseEntity<Humor>(humor, HttpStatus.OK);
-	}
-	
-	@RequestMapping(value = "/updateHumor", method = RequestMethod.POST)
-	public void updateHumor(@RequestBody Humor humor) throws Exception{
-		logger.info("-------------updateHumor-------------" + new Date());
-		Humor h = humorService.selectHumorByNum(humor.getHumorPK());
-		System.out.println(h);
-		System.out.println(humor);
-		boolean flag = humorService.updateHumor(humor);
-		System.out.println(humor);
-		if(!flag) {
-			System.out.println("Empty!!!");
-//			return new ResponseEntity(HttpStatus.NO_CONTENT);
-		}
-//		return new ResponseEntity<Humor>(humor, HttpStatus.OK);
-	}
-	
-	@RequestMapping(value = "/deleteHumor/{humorPK}", method = RequestMethod.DELETE)
-	public void deleteH(@PathVariable int humorPK) throws Exception {
-		logger.info("-------------deleteHumor-----------------------------"+new Date());
-		
-		boolean flag = humorService.deleteHumor(humorPK);
-		System.out.println(flag + "삭제 결과!");
-	}
-	
-	@RequestMapping(value = "/createHumor", method = RequestMethod.PUT)
-	public void createA(@RequestBody Humor humor) throws Exception {
-		logger.info("-------------createA-----------------------------"+new Date());
-		boolean flag= humorService.addHumor(humor);
-	
-=======
-	@Autowired
-	private MemberService  memberService;
-
-	@RequestMapping(value = "/addMember", method = RequestMethod.PUT)
-	public ResponseEntity<BoolResult> addMember(@RequestBody Member member) throws Exception {
-		logger.info("1. -------------addMember-------------"+member+" : "+new Date());
-
-		boolean total = memberService.addMember(member);
-		BoolResult nr = new BoolResult();
-		nr.setCount(total);
-		nr.setName("addMember");
-		nr.setState("success");
-		if (!total) {
-			return new ResponseEntity(HttpStatus.NO_CONTENT);
-		}
-		return new ResponseEntity<BoolResult>(nr, HttpStatus.OK);
->>>>>>> branch 'master' of https://github.com/sej226/spring-boot.git
-	}
-	
-	/*
-	@RequestMapping(value = "/deleteQnA/{qnaPK}/{id}", method = RequestMethod.DELETE)
-	public ResponseEntity<BoolResult> deleteQnA(@PathVariable int qnaPK, @PathVariable String id) throws Exception {
-		logger.info("1. -------------deleteQnA-----------------------------"+new Date());
-		logger.info("1. -------------deleteQnA-----------------------------"+qnaPK);
-		logger.info("1. -------------deleteQnA-----------------------------"+id);
-		QnA qna = qnaService.findQnAByPk(qnaPK);
-		System.out.println(qna);
-		boolean total = true;
-		BoolResult nr = new BoolResult();
-		if(qna.getId().equals(id) && id.equals("admin")) {
-			total = qnaService.deleteQnA(qnaPK);
-			nr.setCount(total);
-			nr.setName("deleteQnA");
-			nr.setState("succ");
-		}else {
-			nr.setCount(total);
-			nr.setName("deleteQnA");
-			nr.setState("false");
-		}
-		
-		if (!total) {
-			return new ResponseEntity(HttpStatus.NO_CONTENT);
-		}
-		return new ResponseEntity<BoolResult>(nr, HttpStatus.OK);
-	}
-	
-	@RequestMapping(value = "/findAllQnA", method = RequestMethod.GET)
-	public ResponseEntity<List<QnA>> findAllQnA() throws Exception {
-		logger.info("1.-------------findAllQnA-----------------------------"+new Date());
-		List<QnA> QnAs = qnaService.findAllQnA();
-		if (QnAs.isEmpty()) {
-			return new ResponseEntity(HttpStatus.NO_CONTENT);
-		}
-		return new ResponseEntity<List<QnA>>(QnAs, HttpStatus.OK);
-	}
-	
-	@RequestMapping(value = "/findQnAById/{id}", method = RequestMethod.GET)
-	public ResponseEntity<List<QnA>> findQnAById(@PathVariable String id) throws Exception {
-		logger.info("1.-------------findQnAById-----------------------------"+new Date());
-		List<QnA> QnAs = qnaService.findQnAById(id);
-		if (QnAs.isEmpty()) {
-			return new ResponseEntity(HttpStatus.NO_CONTENT);
-		}
-		return new ResponseEntity<List<QnA>>(QnAs, HttpStatus.OK);
-	}
-	
-	@RequestMapping(value = "/findQnAByContentAndTitle/{keyword}", method = RequestMethod.GET)
-	public ResponseEntity<List<QnA>> findQnAByContentAndTitle(@PathVariable String keyword) throws Exception {
-		logger.info("1.-------------findQnAByContentAndTitle-----------------------------"+new Date());
-		logger.info("1.-------------findQnAByContentAndTitle-----------------------------"+keyword);
-		List<QnA> QnAs = qnaService.findQnAByContentAndTitle(keyword);
-		System.out.println(keyword);
-		if (QnAs.isEmpty()) {
-			return new ResponseEntity(HttpStatus.NO_CONTENT);
-		}
-		return new ResponseEntity<List<QnA>>(QnAs, HttpStatus.OK);
-	}
-	
-	@RequestMapping(value = "/addAnswer", method = RequestMethod.PUT)
-	public ResponseEntity<BoolResult> addAnswer(@RequestBody QnA qna) throws Exception {
-		logger.info("1.-------------addAnswer-----------------------------"+new Date());
-		logger.info("1.-------------addAnswer-----------------------------"+qna);
-		boolean total = qnaService.addAnswer(qna);
-		BoolResult nr = new BoolResult();
-		nr.setCount(total);
-		nr.setName("addAnswer");
-		nr.setState("succ");
-		if (!total) {
-			return new ResponseEntity(HttpStatus.NO_CONTENT);
-		}
-		return new ResponseEntity<BoolResult>(nr, HttpStatus.OK);
-	}
-	
-	@RequestMapping(value = "/deleteAnswer", method = RequestMethod.PUT)
-	public ResponseEntity<BoolResult> deleteAnswer(@RequestBody QnA qna) throws Exception {
-		logger.info("1.-------------deleteAnswer-----------------------------"+new Date());
-		boolean total = qnaService.deleteAnswer(qna);
-		BoolResult nr = new BoolResult();
-		nr.setCount(total);
-		nr.setName("deleteAnswer");
-		nr.setState("succ");
-		if (!total) {
-			return new ResponseEntity(HttpStatus.NO_CONTENT);
-		}
-		return new ResponseEntity<BoolResult>(nr, HttpStatus.OK);
-	}
-	
-	@RequestMapping(value = "/countQnA", method = RequestMethod.GET)
-	public ResponseEntity<Integer> countQnA() throws Exception {
-		logger.info("1.-------------countQnA-----------------------------"+new Date());
-		Integer count = null;
-		count = qnaService.countQnA();
-		if (count == null) {
-			return new ResponseEntity(HttpStatus.NO_CONTENT);
-		}
-		return new ResponseEntity<Integer>(count, HttpStatus.OK);
-	}*/
+      boolean total = humorService.addHumor(humor);
+      BoolResult nr = new BoolResult();
+      nr.setCount(total);
+      nr.setName("addHumor");
+      nr.setState("success");
+      if (!total) {
+         return new ResponseEntity(HttpStatus.NO_CONTENT);
+      }
+      return new ResponseEntity<BoolResult>(nr, HttpStatus.OK);
+   }
+   @RequestMapping(value = "/selectAllHumor", method = RequestMethod.GET)
+   public ResponseEntity<List<Humor>> selectAllHumor() throws Exception {
+      logger.info("1.-------------selectAllHumor-------------"+new Date());
+      List<Humor> humors = humorService.selectAllHumor();
+      if (humors.isEmpty()) {
+         return new ResponseEntity(HttpStatus.NO_CONTENT);
+      }
+      return new ResponseEntity<List<Humor>>(humors, HttpStatus.OK);
+   }
+   
+   @RequestMapping(value = "/selectHumorByNum/{humorPK}", method = RequestMethod.GET)
+   public ResponseEntity<Humor> selectHumorByNum(@PathVariable int humorPK) throws Exception{
+      logger.info("-------------selectHumorByNum-------------"+new Date());
+      Humor humor = humorService.selectHumorByNum(humorPK);
+      if(humor == null) {
+         return new ResponseEntity(HttpStatus.NO_CONTENT);
+      }
+      return new ResponseEntity<Humor>(humor, HttpStatus.OK);
+   }
+   
+   @RequestMapping(value = "/updateHumor", method = RequestMethod.POST)
+   public void updateHumor(@RequestBody Humor humor) throws Exception{
+      logger.info("-------------updateHumor-------------" + new Date());
+      Humor h = humorService.selectHumorByNum(humor.getHumorPK());
+      System.out.println(h);
+      System.out.println(humor);
+      boolean flag = humorService.updateHumor(humor);
+      System.out.println(humor);
+      if(!flag) {
+         System.out.println("Empty!!!");
+      }
+   }
+   
+   @RequestMapping(value = "/deleteHumor/{humorPK}", method = RequestMethod.DELETE)
+   public void deleteH(@PathVariable int humorPK) throws Exception {
+      logger.info("-------------deleteHumor-----------------------------"+new Date());
+      
+      boolean flag = humorService.deleteHumor(humorPK);
+      System.out.println(flag + "삭제 결과!");
+   }
+   
+   @RequestMapping(value = "/createHumor", method = RequestMethod.PUT)
+   public void createA(@RequestBody Humor humor) throws Exception {
+      logger.info("-------------createA-----------------------------"+new Date());
+      boolean flag= humorService.addHumor(humor);
+   
+   }
+   
+   
 }
