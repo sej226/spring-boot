537c201d1dd4ac4d4013acab338ad9e670265fa8
MoonYongSeong <forever9882@gmail.com>
2019-06-10 15:03:52 +0900

    update memo controller

---
+++ src/main/java/com/boot/controller/MemoController.java
@@ -0,0 +1,85 @@
+package com.boot.controller;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.boot.service.MemberService;
+import com.boot.service.MemoService;
+import com.boot.vo.BoolResult;
+import com.boot.vo.Humor;
+import com.boot.vo.Member;
+import com.boot.vo.Memo;
+
+@Controller
+@RequestMapping("/memo")
+public class MemoController {
+public static final Logger logger = LoggerFactory.getLogger(MemberController.class);
+	
+	@Autowired
+	private MemoService  memoService;
+	
+	@RequestMapping(value = "/send", method = RequestMethod.POST)
+	public ResponseEntity<BoolResult> sendMemo(@RequestBody Memo memo) throws Exception {
+		logger.info("1. -------------sendMemo-------------"+memo+" : "+new Date());
+
+		boolean total = memoService.sendMemo(memo);
+		BoolResult nr = new BoolResult();
+		nr.setCount(total);
+		nr.setName("sendMemo");
+		nr.setState("succ");
+		if (!total) {
+			return new ResponseEntity(HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<BoolResult>(nr, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/delete/{memoPK}", method = RequestMethod.DELETE)
+	public ResponseEntity<BoolResult> deleteMemo(@PathVariable int memoPK) throws Exception {
+		logger.info("1. -------------deleteMemo-------------"+memoPK+" : "+new Date());
+
+		boolean total = memoService.deleteMemo(memoPK);
+		BoolResult nr = new BoolResult();
+		nr.setCount(total);
+		nr.setName("deleteMemo");
+		nr.setState("succ");
+		if (!total) {
+			return new ResponseEntity(HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<BoolResult>(nr, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/selectID", method = RequestMethod.GET)
+	public ResponseEntity<List<Memo>> selectID(HttpSession session) throws Exception {
+		logger.info("1. -------------selectID------------- : "+new Date());
+		Member member = (Member) session.getAttribute("member");
+		List<Memo> memos = memoService.selectMemoByReceiverID(member.getId());
+
+	      if (memos.isEmpty()) {
+	         return new ResponseEntity(HttpStatus.NO_CONTENT);
+	      }
+	      return new ResponseEntity<List<Memo>>(memos, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/selectID/{memoPK}", method = RequestMethod.GET)
+	public ResponseEntity<Memo> selectPK(@PathVariable int memoPK) throws Exception {
+		logger.info("1. -------------selectPK-------------"+memoPK+" : "+new Date());
+		Memo memo = memoService.selectMemoByMemoPK(memoPK);
+		if (memo == null) {
+	         return new ResponseEntity(HttpStatus.NO_CONTENT);
+	      }
+	      return new ResponseEntity<Memo>(memo, HttpStatus.OK);
+	}
+}
